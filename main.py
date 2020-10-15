a7e80f96a4854abfe84be4794157a701076dcbf8
VivianSolide <vivian.sarazin@gmail.com>
2020-10-15 11:18:23 +0200

    setupDone

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def puzzle_solver(pieces, width, height):
     result = []
 
-    def convert(list): 
-        return tuple(list)
+    # def convert(list): 
+    #     return tuple(list)
     
     print('-----------------')
     print("WIDTH:", width, "HEIGHT:", height)
